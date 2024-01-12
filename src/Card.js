623fb946d311eaeab228802b7995d48f7029194a
JC Choi <jobechoi@gmail.com>
2024-01-12 15:59:00 -0800

    urgent mister whiskers

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,20 @@
 import './Card.css'
 import React from 'react';
 
-const Card = ({ image, options, onSelect }) => {
+const Card = ({ image, options, breedName, onSelect }) => {
   return (
     <div className="card">
+      <p>What breed is/are these dogs?</p>
       <img src={image} alt="Dog" className="card-image" />
       <select className="dropdown" onChange={onSelect}>
-        {options.map((option, index) => (
-          <option key={index}>{option}</option>
-        ))}
+        <option value="" disabled selected>Select a breed</option>
+          {options.map((option, index) => (
+            <option key={index}>{option}</option>        
+          ))}
       </select>
     </div>
   );
 };
+
 
 export default Card;
